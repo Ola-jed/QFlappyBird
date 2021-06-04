6e08596a880aa76db655cb4bca54bfe2cbfb901c
olabijed@gmail.com <olabijed@gmail.com>
2021-06-04 08:25:10 +0100

    DAO created but not finished

--- src/scoredao.cpp
+++ src/scoredao.cpp
@@ -1,1 +1,49 @@
 #include "scoredao.hpp"
+
+ScoreDAO::ScoreDAO()
+{
+    myDatabase = QSqlDatabase::addDatabase(DATABASE_TYPE);
+    myDatabase.setDatabaseName(DATABASE_NAME);
+    if(myDatabase.open())
+    {
+        QSqlQuery queryCreation{myDatabase};
+        queryCreation.exec("CREATE TABLE IF NOT EXISTS Stats ( Id INTEGER PRIMARY KEY AUTOINCREMENT,score INTEGER,moment VARCHAR);");
+    }
+}
+
+ScoreDAO::~ScoreDAO()
+{
+    myDatabase.close();
+}
+
+void ScoreDAO::insertScore(int scoreToAdd)
+{
+    QSqlQuery insertQuery{myDatabase};
+    insertQuery.prepare("INSERT INTO Stats(score,moment)"
+                        "VALUES (:score,:moment)");
+    insertQuery.bindValue(":score",scoreToAdd);
+    insertQuery.bindValue(":moment",QDateTime::currentDateTime().toString());
+    insertQuery.exec();
+}
+
+QList<int> ScoreDAO::getAll()
+{
+    QList<int> scoresLoaded{};
+    auto queryAll = myDatabase.exec("SELECT score FROM Stats");
+    while(queryAll.next())
+    {
+        scoresLoaded.push_back(queryAll.value(0).toInt());
+    }
+    return scoresLoaded;
+}
+
+int ScoreDAO::getMaxScore()
+{
+    auto queryBest = myDatabase.exec("SELECT MAX(score) FROM Stats");
+    // We want to be on a valid record
+    queryBest.first();
+    auto const best{queryBest.value(0).toInt()};
+    return best;
+}
+
+
--- src/scoredao.hpp
+++ src/scoredao.hpp
@@ -6,12 +6,23 @@
 #include <QSqlQuery>
 #include <QSqlError>
 #include <QSqlResult>
+#include <QDateTime>
 #include <QSqlDriver>
 #include <QSqlDatabase>
 
 class ScoreDAO
 {
+public:
+    ScoreDAO();
+    ~ScoreDAO();
+    void insertScore(int scoreToAdd);
+    QList<int> getAll();
+    int getMaxScore();
 
+private:
+    const QString DATABASE_TYPE{"QSQLITE"};
+    const QString DATABASE_NAME{"QFlappyBird_Scores.db"};
+    QSqlDatabase myDatabase;
 };
 
 
